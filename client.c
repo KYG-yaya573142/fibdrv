27422d572b1e37514ed78eecdf0017b297b99e61
KY Weng <yaya573142@gmail.com>
2020-05-28 22:26:06 +0800

    Develop and implement the bignum API
    By using the bignum APIs, fibdrv now can calculate to at least 300-th
    fibonacci number (the maximun available number depends on avilable
    memory). The bignum APIs is defined in bn_kernel.h, and related source
    code is in bn_kernel.c.
    The read file operation to /dev/fibonacci will read the n-th fibonacci
    number to user buffer (in decimal string format), and return number of
    bytes that could not be copied.

--- client.c
+++ client.c
@@ -9,11 +9,9 @@
 
 int main()
 {
-    long long sz;
-
-    char buf[1];
-    char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
+    char buf[100];
+    // char write_buf[] = "testing writing";
+    int offset = 300;
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -22,26 +20,11 @@ int main()
     }
 
     for (int i = 0; i <= offset; i++) {
-        sz = write(fd, write_buf, strlen(write_buf));
-        printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
-    }
-
-    for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
-    }
-
-    for (int i = offset; i >= 0; i--) {
-        lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+        long long sz = read(fd, buf, 100);
+        if (sz)
+            printf("returned message was truncated!\n");
+        printf("fib(%d): %s\n", i, buf);
     }
 
     close(fd);
--- fibdrv.c
+++ fibdrv.c
@@ -6,6 +6,10 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>  // Required for the copy_to_user()
+
+#include "bn_kernel.h"
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("National Cheng Kung University, Taiwan");
@@ -17,7 +21,7 @@ MODULE_VERSION("0.1");
 /* MAX_LENGTH is set to 92 because
  * ssize_t can't fit the number > 92
  */
-#define MAX_LENGTH 92
+#define MAX_LENGTH 300
 
 static dev_t fib_dev = 0;
 static struct cdev *fib_cdev;
@@ -89,14 +93,16 @@ static ssize_t fib_read(struct file *file,
                         size_t size,
                         loff_t *offset)
 {
-    switch (size) {
-    case 0:
-        return (ssize_t) fib_sequence(*offset);
-    case 1:
-        return (ssize_t) fib_sequence_fdouble(*offset);
-    default:
-        return 0;
-    }
+    bn *fib = bn_alloc(1);
+    bn_fib_fdoubling(fib, *offset);
+    // bn_fib(fib, *offset);
+    char *p = bn_to_string(fib);
+    size_t len = strlen(p) + 1;
+    size_t left = copy_to_user(buf, p, len);
+    // printk(KERN_DEBUG "fib(%d): %s\n", (int) *offset, p);
+    bn_free(fib);
+    kfree(p);
+    return left;  // return number of bytes that could not be copied
 }
 
 /* write operation actually returns the time spent on
